d9b22691213472a9c0d5e2eec1a26e36053012df
emmanuelO22 <emmanuelodioko22@gmail.com>
2025-01-20 00:02:17 +0100

    Github workflows added 2

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  productionBrowserSourceMaps: false, // Add this to disable source maps in production
-  webpack: (config, { isServer, dev }) => {
-    // Disable source maps in production
-    if (!dev) {
-      config.devtool = false;
-    }
-    return config;
-  },
+  productionBrowserSourceMaps: false,
 };
 
 export default nextConfig;
