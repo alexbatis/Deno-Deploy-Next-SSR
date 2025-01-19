f1ef9f3ca3b213d8ccf5992080be299db58c4137
emmanuelO22 <emmanuelodioko22@gmail.com>
2025-01-19 23:48:24 +0100

    Fixing map fucntion error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
+  productionBrowserSourceMaps: false, // Add this to disable source maps in production
+  webpack: (config, { isServer, dev }) => {
+    // Disable source maps in production
+    if (!dev) {
+      config.devtool = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
