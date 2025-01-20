b78257820d0c6b09124f5f08c281e8656f9720e0
emmanuelO22 <emmanuelodioko22@gmail.com>
2025-01-20 22:22:08 +0100

    Next SSR With Deno Deploy completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-
-
-export default async function Page() {
+export async function getServerSideProps() {
   const joke = await fetch('https://api.chucknorris.io/jokes/random')
     .then(res => res.json());
 
+  return {
+    props: { joke }
+  };
+}
+
+export default function Page({ joke }) {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
       <div className="max-w-2xl w-full bg-white rounded-lg shadow-lg p-8">
@@ -14,5 +18,3 @@ export default async function Page() {
     </div>
   );
 }
-
-
